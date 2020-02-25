7cc379ec0d01f3f1a07396ff2ac6c170785df57b
Shashank Shekhar <shashank7200@gmail.com>
2020-02-26 03:29:41 +0530

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FiSun } from 'react-icons/fi'
+import { FaMoon } from 'react-icons/fa'
+
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+
+
+function Header() {
+  const theme = 'night';
+
+  return (
+    <header
+      css={css`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        background-color: #eee;
+        padding: 32px;
+    `}>
+      <div className="header-title">
+        Markdown Editor
+      </div>
+      <div>
+       {
+         theme === 'night'?
+         <FaMoon />:
+         <FiSun />
+       }
+      </div>
+    </header>
+  )
+}
+
+export default Header;
