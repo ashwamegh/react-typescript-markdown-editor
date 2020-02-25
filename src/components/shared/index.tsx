d4eecf861bc68c1151c06af4c22c910631ed0f76
Shashank Shekhar <shashank7200@gmail.com>
2020-02-26 04:16:21 +0530

    created file for shared components

---
+++ src/components/shared/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+export const ColumnFlex = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const RowFlex = styled.div`
+  display: flex;
+  flex-direction: row;
+`
