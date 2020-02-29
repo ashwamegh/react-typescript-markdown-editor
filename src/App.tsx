b90d1e2b0b70c14699e9855ea35b67b25c173b7b
Shashank Shekhar <shashank7200@gmail.com>
2020-03-01 02:05:46 +0530

    renamed Editor component to Main

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 // Components
 import Header from './components/Header'
-import Editor from './components/Editor'
+import Main from './components/Main'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Main />
     </div>
   );
 }
